d9795f781be0f91755651ca42de8527ef96ba206
Kajal Kumari <99003849+Kajal0332@users.noreply.github.com>
2024-07-19 13:57:26 +0530

    To-Do list Application
    This is a To-Do list application with a responsive design.
    If you like it please give it start.

---
+++ ToDoList/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $("#btn").click(function add() {
+      // $("ul li").on("click", function (event) {
+      //   if (confirm("delete?")) {
+      //     event.target.parentElement.remove();
+      //   }
+      // });
+      var input = $("#input").val().trim();
+    if (input) {
+      $("ul").append(
+        "<li>" +
+          input +
+          '<i class="fa-solid fa-trash"></i>' +'<i class="fa-solid fa-check"></i>',
+        "</li>"
+      );
+        $(this).val("");
+        $("ul").on("click", ".fa-trash", function () {
+          $(this).parent("li").hide();
+        });
+        $("ul").on("click", ".fa-check", function () {
+          $(this).parent("li").toggleClass("checked");
+        });
+      } else {
+        alert("please enter something!");
+      }
+    });
+  });
