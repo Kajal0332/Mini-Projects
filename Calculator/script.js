550df6cda7688ab2a535eb99853ae71eafc98027
Kajal Kumari <99003849+Kajal0332@users.noreply.github.com>
2024-07-19 13:59:43 +0530

    Calculator
    This is a calculator application for desktops with a responsive design.

---
+++ Calculator/script.js
@@ -0,0 +1,41 @@
+
+
+
+// let arr = Array.from(buttons);
+// arr.forEach(button => {
+//     button.addEventListener("click", (e) => {
+//         if (e.target.innerHtml == '=') {
+//             string = eval(string);
+//             input.value = string;
+//         }
+        
+//         string += e.target.innerHTML;
+//         input.value = string;
+//         // if (e.target.innerHTML == '=') {
+//         //     string = eval(string);
+//         //     document.querySelector(".input").value = string;
+//         // }
+//         // console.log(e.target)
+//         // string = string + e.target.innerHTM;
+//         // document.querySelector("input").value = string;
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+let x = document.querySelector("#clear");
+let y = document.querySelector("#equal");
+let z = document.querySelector("#divid");
+let a = document.querySelector("#zero");
+
+
